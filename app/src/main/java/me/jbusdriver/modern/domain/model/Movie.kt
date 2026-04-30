286085a221d1fbfbd9ac9f32262e8d223a14cfa5
Mupceet <mupceet@gmail.com>
2026-04-30 20:52:24 +0800

    fix: remove @Transient from Movie.tags to preserve tags through cache serialization

--- app/src/main/java/me/jbusdriver/modern/domain/model/Movie.kt
+++ app/src/main/java/me/jbusdriver/modern/domain/model/Movie.kt
@@ -15,7 +15,7 @@ data class Movie(
     val code: String, //番号
     val date: String, //日期
     @SerializedName("detailUrl") override val link: String,
-    @Transient val tags: List<String>? = listOf()//标签,
+    val tags: List<String>? = listOf(),
 
 ) : ILink {
     @Transient
