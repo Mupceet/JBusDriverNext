2d4422c38087bf4e4b596b678ca1fe29b973e8c1
Mupceet <mupceet@gmail.com>
2026-05-20 01:05:01 +0800

    feat: add forum domain models for forum browsing feature
    Add ForumBoard, ForumThread, ForumThreadDetail, ForumReply, Comment,
    ForumTypeFilter, and ForumThreadPageResult data classes to support the
    forum browsing feature.

---
+++ app/src/main/java/me/jbusdriver/modern/domain/model/ForumModels.kt
@@ -0,0 +1,97 @@
+package me.jbusdriver.modern.domain.model
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class ForumBoard(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val todayPosts: Int,
+    val totalThreads: String,
+    val totalPosts: String,
+    val lastPost: LastPost
+)
+
+@Immutable
+data class LastPost(
+    val title: String,
+    val author: String,
+    val time: String
+)
+
+@Immutable
+data class ForumThread(
+    val tid: Int,
+    val typeId: Int,
+    val typeName: String,
+    val typeColor: String,
+    val title: String,
+    val author: String,
+    val authorUid: Int,
+    val authorAvatar: String,
+    val dateLine: String,
+    val viewCount: Int,
+    val replyCount: Int,
+    val lastReplyAuthor: String,
+    val lastReplyTime: String,
+    val images: List<String>,
+    val isPinned: Boolean,
+    val isDigest: Boolean,
+    val pages: Int
+)
+
+@Immutable
+data class ForumThreadDetail(
+    val tid: Int,
+    val typeId: Int,
+    val typeName: String,
+    val typeColor: String,
+    val title: String,
+    val viewCount: Int,
+    val replyCount: Int,
+    val author: String,
+    val authorUid: Int,
+    val authorAvatar: String,
+    val postTime: String,
+    val content: String,
+    val contentImages: List<String>,
+    val comments: List<Comment>,
+    val replies: List<ForumReply>,
+    val pageInfo: PageInfo
+)
+
+@Immutable
+data class Comment(
+    val author: String,
+    val authorAvatar: String,
+    val content: String,
+    val time: String
+)
+
+@Immutable
+data class ForumReply(
+    val floor: Int,
+    val author: String,
+    val authorUid: Int,
+    val authorAvatar: String,
+    val authorGroup: String,
+    val content: String,
+    val contentImages: List<String>,
+    val postTime: String
+)
+
+@Immutable
+data class ForumTypeFilter(
+    val typeId: Int,
+    val name: String,
+    val color: String,
+    val count: Int
+)
+
+@Immutable
+data class ForumThreadPageResult(
+    val threads: List<ForumThread>,
+    val pageInfo: PageInfo,
+    val typeFilters: List<ForumTypeFilter>
+)
