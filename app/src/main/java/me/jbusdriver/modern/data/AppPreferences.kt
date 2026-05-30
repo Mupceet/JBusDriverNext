76ed3efe1cfe68592dec065c541bb9db89ef5518
Mupceet <mupceet@gmail.com>
2026-05-30 23:38:32 +0800

    refactor: delete PreferencesModule, AppPreferences, CoverStats (dead code)

--- app/src/main/java/me/jbusdriver/modern/data/AppPreferences.kt
+++
@@ -1,9 +0,0 @@
-package me.jbusdriver.modern.data
-
-object AppPreferences {
-    const val LAB_SETTINGS = "lab_settings"
-    const val SEARCH_HISTORY = "search_history"
-    const val SESSION_COOKIES = "session_cookies"
-    const val UI_PREFS = "ui_prefs"
-    const val GIF_LOADED_URLS = "gif_loaded_urls"
-}
